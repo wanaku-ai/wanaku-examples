dd7ebb03c86c76b449627fa33cab0b1a9bed8941
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-04-09 10:01:12 +0200

    Initial example showing a client for Jolokia

---
+++ tools/wanaku-tool-service-jolokia/src/main/java/ai/wanaku/tool/InvocationService.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.wanaku.routing.service;
+
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+
+import ai.wanaku.core.exchange.InquireReply;
+import ai.wanaku.core.exchange.InquireRequest;
+import ai.wanaku.core.exchange.Inquirer;
+import ai.wanaku.core.exchange.InvocationDelegate;
+import ai.wanaku.core.exchange.ToolInvokeReply;
+import ai.wanaku.core.exchange.ToolInvokeRequest;
+import ai.wanaku.core.exchange.ToolInvoker;
+import ai.wanaku.core.service.discovery.util.DiscoveryUtil;
+import ai.wanaku.core.services.config.WanakuToolConfig;
+import io.quarkus.grpc.GrpcService;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.scheduler.Scheduled;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.common.annotation.Blocking;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+@GrpcService
+public class InvocationService implements ToolInvoker, Inquirer {
+    private static final Logger LOG = Logger.getLogger(InvocationService.class);
+
+    @Inject
+    InvocationDelegate delegate;
+
+    @Inject
+    WanakuToolConfig config;
+
+    @ConfigProperty(name = "quarkus.grpc.server.port")
+    int port;
+
+    @Override
+    @Blocking
+    public Uni<ToolInvokeReply> invokeTool(ToolInvokeRequest request) {
+        return Uni.createFrom().item(() -> delegate.invoke(request));
+    }
+
+    @Override
+    public Uni<InquireReply> inquire(InquireRequest request) {
+        InquireReply reply = InquireReply.newBuilder()
+                .putAllServiceConfigurations(delegate.serviceConfigurations())
+                .putAllCredentialsConfigurations(delegate.credentialsConfigurations())
+                .build();
+
+        return Uni.createFrom().item(() -> reply);
+    }
+
+    @Scheduled(every="{wanaku.service.tool.registration.interval}", delayed = "{wanaku.service.tool.registration.delay-seconds}", delayUnit = TimeUnit.SECONDS)
+    void register() {
+        delegate.register();
+    }
+
+    void deregister(@Observes ShutdownEvent ev) {
+        LOG.info("De-registering tool service");
+
+        delegate.deregister(config.name(), DiscoveryUtil.resolveRegistrationAddress(), port);
+    }
+}
---
+++ tools/wanaku-tool-service-jolokia/src/main/java/ai/wanaku/tool/JolokiaDelegate.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.wanaku.routing.service;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import ai.wanaku.api.exceptions.InvalidResponseTypeException;
+import ai.wanaku.api.exceptions.NonConvertableResponseException;
+import ai.wanaku.core.services.tool.AbstractToolDelegate;
+
+
+@ApplicationScoped
+public class JolokiaDelegate extends AbstractToolDelegate {
+
+    @Override
+    protected List<String> coerceResponse(Object response) throws InvalidResponseTypeException, NonConvertableResponseException {
+        if (response == null) {
+            throw new InvalidResponseTypeException("Invalid response type from the consumer: null");
+        }
+
+        return List.of((String) response);
+    }
+}
